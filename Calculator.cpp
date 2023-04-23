338d6066c4456b847e21fd823c89aa2bb29486a6
MrTonan <69697169+MrTonan@users.noreply.github.com>
2023-04-23 22:30:56 +0300

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -3,6 +3,52 @@
 
 int main()
 {
+	long double a = 0, b = 0;
+	char c = '!';
+	std::string result = "Result of ";
+
+	std::cout << "[Input1][Operator][Input2] :";
+	std::cin >> a;
+	do
+	{
+		std::cin >> c >> b;
+		switch (c)
+		{
+		case('+'):
+			a +=b ;
+			result.append("Addition:       ");
+			break;
+		case('-'):
+			a -= b;
+			result.append("Subtraction:    ");
+			break;
+		case('*'):
+			a *= b;
+			result.append("Multiplication: ");
+			break;
+		case('/'):
+			a /= b;
+			result.append("Division:       ");
+		default:
+			break;
+		}
+		if (c == '/' && b == 0)
+		{
+			break;
+		}
+		result.append(std::to_string(a));
+		std::cout << result;
+		result = "Result of ";
+	} while (true);
+}
+
+/* Previous Version
+
+#include <iostream>
+#include <string>
+
+int main()
+{
 	long a = 0, b = 0;
 	char c = '!';
 	std::string result = "Result of ";
@@ -32,3 +78,4 @@ int main()
 		break;
 	}
 }
+*/

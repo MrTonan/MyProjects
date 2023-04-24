99bc5910d4ef242db84cde7e4b64074a968d0129
MrTonan <69697169+MrTonan@users.noreply.github.com>
2023-04-24 18:31:53 +0300

    Update Source.cpp

--- Inventory/Source.cpp
+++ Inventory/Source.cpp
@@ -5,10 +5,11 @@ int main()
 {
 	Inventory inv;
 	char input = '0';
-	short int amount = 0;
+	unsigned short amount = 0;
 	char name[16];
+	unsigned short slot = 0;
 
-	std::cout << "Hello" << std::endl;
+	std::cout << "Starting..." << std::endl;
 
 	while (true)
 	{
@@ -17,26 +18,37 @@ int main()
 			break;
 		}
 		std::cout << "Functions: " << std::endl;
-		std::cout << " 1. Open Inventory" << std::endl;
-		std::cout << " 2. Add item to inventory" << std::endl;
-		std::cout << " 3. Clear inventory" << std::endl;
+		std::cout << " 1.    Open Inventory" << std::endl;
+		std::cout << " 2.    Add item to slot" << std::endl;
+		std::cout << " 3.    Remove item to slot" << std::endl;
+		std::cout << " 4.    Clear inventory" << std::endl;
 		std::cout << " Else. Exit" << std::endl;
+		std::cout << "";
 
 		std::cin >> input;
 
 		switch (input)
 		{
 		case('1'):
+			std::cout << "------------------------------" << std::endl;
 			inv.seeInventory();
+			std::cout << "------------------------------" << std::endl;
 			break;
 		case('2'):
-			std::cout << "Enter item's name: ";
+			std::cout << "- Enter item's name: ";
 			std::cin >> name;
-			std::cout << "Enter item's amount: ";
+			std::cout << "- Enter item's amount: ";
 			std::cin >> amount;
-			inv.addToInventory(amount, name);
+			std::cout << "- Enter item's slot [1-10, else last empty slot]: ";
+			std::cin >> slot;
+			inv.addToInventory(amount, name, slot-1);
 			break;
 		case('3'):
+			std::cin >> slot;
+			std::cout << "- Enter item's slot: ";
+			inv.removeInventorySlot(slot);
+			break;
+		case('4'):
 			inv.clearInventory();
 			break;
 		default:
